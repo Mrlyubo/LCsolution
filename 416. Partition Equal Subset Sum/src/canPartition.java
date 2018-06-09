2d9405e6428e2985c5786a1e977e98ccad98c778
LyuBo <1150671@tongji.edu.cn>
2018-06-09 15:26:21 +0800

    2018.6.9

--- 416. Partition Equal Subset Sum/src/canPartition.java
+++ 416. Partition Equal Subset Sum/src/canPartition.java
@@ -3,22 +3,35 @@ class A {
         int sum = 0;
         for(int n: nums)
             sum += n;
-        return sum%2 != 0 ? false : cansplit(nums, sum/2);
+        System.out.println ("sum % 2 =" + sum%2);
+        return
+                sum%2 != 0 ?
+                false :
+                cansplit(nums, sum/2);
     }
     private boolean cansplit( int[] nums, int Sum ){
-        int[] dp = new int[Sum+1];
+        long[] dp = new long[Sum+1];
         dp[0] = 1;
         for(int n: nums)
             for(int i = Sum; i >= n; i--)
                 dp[i] += dp[i - n];
-        return dp[Sum] > 0;
+        System.out.println(dp[Sum]);
+        return
+                dp[Sum] != 0; // Don't use dp[Sum] >0 , it may cause int overflow.
     }
     public static void main(String [] args){
         A Launcher = new A();
         Launcher.start();
     }
     public void start(){
-        Strin
+        int[] nums ={100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+                     100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+                     100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+                     100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100};
         System.out.println(canPartition(nums));
+
+        int[] nums1 ={100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,
+                      100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100};
+        System.out.println(canPartition(nums1));
     }
 }
