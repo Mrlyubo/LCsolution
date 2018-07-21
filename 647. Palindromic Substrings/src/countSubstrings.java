45b35ddecaac48296e44278f4ca1d41e98677a45
LyuBo <1150671@tongji.edu.cn>
2018-07-21 23:53:50 +0800

    2018.7.21

---
+++ 647. Palindromic Substrings/src/countSubstrings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countSubstrings(String s) {
+
+    }
+    public static void main(String[] args) {
+        Solution Launcher = new Solution();
+        Launcher.start();
+    }
+
+    public void start() {
+
+        System.out.println();
+    }
+}
+
