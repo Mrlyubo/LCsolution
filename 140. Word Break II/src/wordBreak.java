f6e6c063129643fbdccb8de8367a135f8ae7975b
mrLyubo <1150671@tongji.edu.cn>
2018-08-17 08:32:19 +0800

    work break 2

---
+++ 140. Word Break II/src/wordBreak.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class wordBreak {
+
+
+    public static void main(String[] args) {
+        wordBreak Launcher = new wordBreak();
+        Launcher.start();
+    }
+
+    public void start() {
+
+    }
+}
